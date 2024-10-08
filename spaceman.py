b794eacdbfa628531520019f9630e91059b493e7
David Doherty <froztydavie@Davids-MacBook-Air-3.local>
2024-10-07 20:16:12 -0700

    spaceman function

--- spaceman.py
+++ spaceman.py
@@ -42,7 +42,44 @@ def spaceman(secret_word):
       secret_word (string): the secret word to guess.
 
     '''
+    print("Welcome to Spaceman!")
+    print("Dave the Spaceman is running out of air to save him you need to guess the secret word.")
+    print(f"The secret word has {len(secret_word)} letters.")
+    print("You can guess one letter at a time.")
+    print("You have a total of 7 incorrect guesses before Dave the Spaceman DIES!!\n")
 
+
+    letters_guessed = []
+    attempts_left = 7
+
+    while attempts_left > 0 and not is_word_guessed(secret_word, letters_guessed):
+        print(f"Secret word: {get_guessed_word(secret_word, letters_guessed)}")
+        print(f"Attempts left: {attempts_left}")
+        print(f"Letters guessed so far: {', '.join(letters_guessed)}")
+
+        guess = input("\nGuess a Letter: ").lower()
+
+        if len(guess) != 1 or not guess.isalpha():
+            print("Invalid input. Please guess a single letter or else Dave will be in trouble.\n")
+            continue
+
+        if guess in letters_guessed:
+            print(f"You already guessed the letter '{guess}'. Try again.\n")
+            continue
+
+        
+        letters_guessed.append(guess)
+
+        if is_guess_in_word(guess, secret_word):
+            print(f"Good guess! The letter '{guess}' is in the word. Dave is one step closer to being saved!\n")
+        else:
+            print(f"Oh No! The letter '{guess}' is incorrect, Dave is running out of air!\n")
+            attempts_left -= 1
+
+    if is_word_guessed(secret_word, letters_guessed):
+        print(f"Congratulations! you've guessed the secret word '{secret_word}' correctly. YOU SAVED DAVE!")
+    else:
+        print(f"Sorry, you ran out of attempts. The secret word was '{secret_word}'. Dave ran out of air!")
 
     #TODO: show the player information about the game according to the project spec
 
